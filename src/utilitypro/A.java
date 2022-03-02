263f05af95d0166f4eda83168fe6cfc7e336e911
Akash patil <100683144+akashpatil001@users.noreply.github.com>
2022-03-02 12:35:02 +0530

    Update A.java

--- src/utilitypro/A.java
+++ src/utilitypro/A.java
@@ -5,6 +5,16 @@ public class A
   public static void main(String[] args) 
   {
 	int a;
-	
+	int fact=1;
+		System.out.println("enter the number=");
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        for(int i=a; i>=1;i--)
+        {
+        	fact=fact*i;
+            
+        }
+        System.out.println("the factorial="+fact);
+        
   }
 }
